6eaa42d34b2ef5e49570880d676761215ff716f5
M1lken01 <xmilken01x@gmail.com>
2022-12-31 22:25:42 +0100

    clock is ticking

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-//let countdownDate = new Date;
-//countdownDate.setTime(1672531200000);
-let targetDate = new Date('2022-12-31T23:00:00');
-let timeZoneDiff = 3600000 // 1 hour
-let countDownStart = 5000 // 5 sec
+const timeZone = +1;
+
+const targetDate = new Date('2022-12-31T24:00:00');
+const timeZoneDiff = 3600000 * timeZone
+const countDownStart = 10000 // 10 sec
 
 updateTime();
 
 function updateTime() {
     let currentDate = new Date();
-    if (targetDate - currentDate <= (timeZoneDiff + countDownStart))
+    let timeLeft = targetDate - currentDate - timeZoneDiff;
+    if (timeLeft <= countDownStart) {
+        document.body.classList.add('countdown');
+    }
+    if (timeLeft <= 0) {
         document.body.classList.add('end');
-    if (targetDate - currentDate <= timeZoneDiff) {
-        console.log('happy new year')
+        console.log('happy new year');
     } else {
         let timer = new Intl.DateTimeFormat('en-GB', {
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
-        }).format(targetDate - currentDate);
+        }).format(timeLeft);
         let timerOutput = document.getElementsByClassName('number');
+        let checks = 0;
         for (let i = 0; i < timerOutput.length; i++) {
             timerOutput[i].innerHTML = timer.split(':')[i];
+            if (i == 0) {
+                checks++;
+            } else if (timerOutput[i - 1].classList.contains('red')) {
+                checks++;
+            }
+            if (timer.split(':')[i] == '00' && !timerOutput[i].classList.contains('red') && checks == i + 1) {
+                timerOutput[i].classList.add('red');
+            }
         }
         setTimeout(function() {
             updateTime()
