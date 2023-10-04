d5176336b43612120362572a796d2edc856581a6
Milan Artner <xmilken01x@gmail.com>
2023-10-04 20:32:41 +0200

    added time

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const timeZone = +1;
 const targetDate = new Date('2023-12-31T24:00:00');
 const timeZoneDiff = 3600000 * timeZone;
 const countDownStart = 4000; // 4 sec
+const countDown = true;
 
 function updateTime() {
-  const timeLeft = targetDate.getTime() - new Date().getTime();
+  const currentTime = new Date();
+  const timeLeft = countDown ? targetDate.getTime() - currentTime.getTime() : currentTime.getTime();
+
+  console.log(timeLeft);
 
   if (timeLeft <= countDownStart) document.body.classList.add('countdown');
   if (timeLeft <= 0) return document.body.classList.add('end');
@@ -24,6 +28,15 @@ function updateTime() {
     timerDisplayNumbers[i].innerHTML = timerArray[i];
     if (timerArray[i] === '00' && !timerDisplayNumbers[i].classList.contains('red') && reds >= i) timerDisplayNumbers[i].classList.add('red');
   }
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  document.getElementById('weekDay').innerHTML = days[currentTime.getDay()];
+  document.getElementById('month').innerHTML = months[currentTime.getMonth()];
+  document.getElementById('monthDay').innerHTML = currentTime.getDate();
+  document.getElementById('year').innerHTML = currentTime.getFullYear();
+
   setTimeout(updateTime, 1000);
 }
 
